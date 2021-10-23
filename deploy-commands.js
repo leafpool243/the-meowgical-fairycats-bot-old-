d7a951cfd82d621b2a6a2d2ef8df549fc65d26a8
Leafpool <63614345+leafpool243@users.noreply.github.com>
2021-10-23 00:16:49 -0700

    Added Chinese hi command and whoami

--- deploy-commands.js
+++ deploy-commands.js
@@ -8,8 +8,9 @@ const commands = [
 	// new SlashCommandBuilder().setName('buttontest').setDescription('Replies with button test.'),
 	// new SlashCommandBuilder().setName('embedtest').setDescription('Creates an embed.'),
 	new SlashCommandBuilder().setName('hi').setDescription('Say hi to the bot!! :3'),
-	new SlashCommandBuilder().setName('你好').setDescription('跟機器人說你好！！ :3'),
 	new SlashCommandBuilder().setName('ping').setDescription('Ping pong!'),
+	new SlashCommandBuilder().setName('whoami').setDescription('[NOT FUNCTIONAL YET] Displays user information!'),
+	new SlashCommandBuilder().setName('你好').setDescription('跟機器人說你好！！ :3'),
 ]
 	.map(command => command.toJSON());
 
