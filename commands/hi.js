589bfe5f1f77527fd0f0ed60a37d1adc3fea7978
Leafpool <63614345+leafpool243@users.noreply.github.com>
2021-10-22 23:18:08 -0700

    Fixed hi command
    Made bot check if the user that ran the command has a nickname. If not, the bot will use the username (without the tag / discriminator).
    
    Previously, when the user did not have a discriminator, it would call them "undefined".

--- commands/hi.js
+++ commands/hi.js
@@ -1,5 +1,5 @@
 function hi(interaction) {
-        interaction.reply(`Hi there, ${interaction.member.nick}! :3`);
+        interaction.reply(`Hi there, ${interaction.member.nick || interaction.user.username}! :3`);
 };
 
 export { hi as default };
